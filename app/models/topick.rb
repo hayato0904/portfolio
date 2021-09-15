88946759ffa77b1b6435de375c6f0dc6935de19c
hayato <sakigi94@gmail.com>
2021-09-15 11:07:52 +0000

    追加;througオプション

--- app/models/topick.rb
+++ app/models/topick.rb
@@ -1,5 +1,7 @@
 class Topick < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   validates :content, presence: true
 end
