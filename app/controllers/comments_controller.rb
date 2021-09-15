a959bad9b3d0e825b32dd4a94f6a8cc8cfd5b4d7
hayato <sakigi94@gmail.com>
2021-09-15 01:26:42 +0000

    追加:comments_controllerにリクエストに応したフォーマットの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,20 @@
 class CommentsController < ApplicationController
   def create
+    # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
+    @topick = Topick.find(params[:topick_id])
+    @comment = @topick.comments.build(comment_params)
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@topick) }
+      else
+        format.html { redirect_to blog_path(@topick), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+  private
+  # ストロングパラメーター
+  def comment_params
+    params.require(:comment).permit(:topick_id, :content)
   end
 end
