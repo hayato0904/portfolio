e336a35feafac1b3c9d1aba67aee0f5049bee466
hayato <sakigi94@gmail.com>
2021-09-16 05:30:09 +0000

    追加:jQueryの導入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
   def create
+    binding.pry
     favorite = current_user.favorites.create(topick_id: params[:topik_id])
     redirect_to topicks_path, notice: "#{favorite.topick.user.name}さんのブログをお気に入り登録しました"
   end
