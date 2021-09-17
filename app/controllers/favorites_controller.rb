7f8ec09c82b9b0af844fdf49ac101e9cf82c2246
hayato <sakigi94@gmail.com>
2021-09-17 07:50:16 +0000

    編集:掲示板に名前を統一

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,10 @@ class FavoritesController < ApplicationController
   def create
     binding.pry
     favorite = current_user.favorites.create(topick_id: params[:topik_id])
-    redirect_to topicks_path, notice: "#{favorite.topick.user.name}さんのブログをお気に入り登録しました"
+    redirect_to topicks_path, notice: "#{favorite.topick.user.name}さんの投稿をお気に入り登録しました"
   end
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to topicks_path, notice: "#{favorite.topick.user.name}さんのブログをお気に入り解除しました"
+    redirect_to topicks_path, notice: "#{favorite.topick.user.name}さんの投稿をお気に入り解除しました"
   end
 end
