761d22a798926005b23b95612422ab1fd2cc5262
hayato <sakigi94@gmail.com>
2021-09-17 14:25:27 +0000

    削除:userテーブルの重複カラムを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :topicks
   has_many :favorites, dependent: :destroy
+  # devise :database_authenticatable, :registerable,
+  # :recoverable, :rememberable
+
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable
+  :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_09_15_102802) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2021_09_15_102802) do
     t.integer "constellation"
     t.string "self_lntroduction"
     t.string "icon"
-    t.string "password_digest"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
