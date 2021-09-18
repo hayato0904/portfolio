591c93e282b38a4fbee545a0ee2d733dfb9efd52
hayato <sakigi94@gmail.com>
2021-09-18 10:53:27 +0000

    追加:seedデータ作成完了

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+    email: email,
+    password: password,
+  )
+end
