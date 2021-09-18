14b05fcfae7d4d17416fbf9a6943267e3bcc5fd8
hayato <sakigi94@gmail.com>
2021-09-18 12:53:46 +0000

    追加:rootをTOP画面にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'tops#index'
   resources :favorites, only: [:create, :destroy]
   get '/users', to: 'users#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get '/topicks', to: 'topicks#index'
-  root 'topicks#index'
   resources :topicks do
     collection do
       post :confirm
