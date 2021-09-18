286de2f588d465a86ad82e00add47c9e28a0e940
hayato <sakigi94@gmail.com>
2021-09-18 12:06:52 +0000

    編集:seedデータに管理者も追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
     password: password,
   )
 end
+
+1.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+    email: email,
+    password: password,
+    admin: true
+  )
+end
