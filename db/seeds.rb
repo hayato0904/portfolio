3d91882211757219c928d0d0f0bad3edfacf6993
hayato <sakigi94@gmail.com>
2021-09-20 10:58:29 +0000

    Revert "herokuに上げれるようにseedデータを代入。"
    This reverts commit 1af6662be6018c86610b8201bb1ed56b2e868801.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-1.times do |n|
+10.times do |n|
   name = Faker::Games::Pokemon.name
-  email = "test1@example.com"
+  email = Faker::Internet.email
   password = "password"
   User.create!(name: name,
     email: email,
@@ -10,37 +10,7 @@ end
 
 1.times do |n|
   name = Faker::Games::Pokemon.name
-  email = "test2@example.com"
-  password = "password"
-  User.create!(name: name,
-    email: email,
-    password: password,
-  )
-end
-
-1.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = "test3@example.com"
-  password = "password"
-  User.create!(name: name,
-    email: email,
-    password: password,
-  )
-end
-
-1.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = "test4@example.com"
-  password = "password"
-  User.create!(name: name,
-    email: email,
-    password: password,
-  )
-end
-
-1.times do |n|
-  name = 'admin'
-  email = 'admin@example.com'
+  email = Faker::Internet.email
   password = "password"
   User.create!(name: name,
     email: email,
