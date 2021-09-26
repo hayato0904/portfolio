6bac60ed270a1add6fe944aea1701282de1b80bc
hayato <sakigi94@gmail.com>
2021-09-26 15:22:03 +0000

    追加:ユーザーのモデルスペックのテスト実装完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,68 @@
 require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  let!(:user) { FactoryBot.create(:user) }
-  let!(:user2) { FactoryBot.create(:user2) }
-  let!(:user3) { FactoryBot.create(:user3) }
-  describe '' do
-    context '' do
-      it '' do
-        expect(second_user).to be_valid
+RSpec.describe 'ユーザモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'ユーザの名前が空の場合' do
+      it 'バリデーションに引っかかる' do
+        user = User.new(name: '', email: 'user@example.com', password: 'password')
+        expect(user).not_to be_valid
       end
     end
-    context '名前が空欄の場合' do
-      it 'バリデーションエラーになる' do
-        second_user.name = nil
-        expect(second_user).to be_invalid
+    context 'ユーザの名前が30字を超える場合' do
+      it 'バリデーションに引っかかる' do
+        user = User.new(name: 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDE', email: 'user@example.com', password: 'password')
+        expect(user).not_to be_valid
       end
     end
+    context 'ユーザの名前が30字の場合' do
+      it 'バリデーションが通る' do
+        user = User.new(name: 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCD', email: 'user@example.com', password: 'password')
+        expect(user).to be_valid
+      end
+    end
+    context 'ユーザのメールアドレスが空の場合' do
+      it 'バリデーションに引っかかる' do
+        user = User.new(name: 'user', email: '', password: 'password')
+        expect(user).not_to be_valid
+      end
+    end
+    context 'ユーザのメールアドレスが255字を超える場合' do
+      it 'バリデーションに引っかかる' do
+        user = User.new(name: 'user',
+                        email: 'ABC@EFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQR.com', password: 'password')
+        expect(user).not_to be_valid
+      end
+    end
+    context 'ユーザのメールアドレスが255字の場合' do
+      it 'バリデーションが通る' do
+        user = User.new(name: 'user',
+                        email: 'ABC@EFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQ.com', password: 'password')
+        expect(user).to be_valid
+      end
+    end
+    context 'ユーザのメールアドレスのフォーマットが不正の場合' do
+      it 'バリデーションが引っかかる' do
+        user = User.new(name: 'user', email: 'user.example.com', password: 'password')
+        expect(user).not_to be_valid
+      end
+    end
+    context 'ユーザのパスワードが空の場合' do
+      it 'バリデーションが引っかかる' do
+        user = User.new(name: 'user', email: 'user@example.com', password: '')
+        expect(user).not_to be_valid
+      end
+    end
+    context 'ユーザのパスワードが6字未満の場合' do
+      it 'バリデーションが引っかかる' do
+        user = User.new(name: 'user', email: 'user@example.com', password: 'passw')
+        expect(user).not_to be_valid
+      end
+    end
+    context 'ユーザのパスワードが255字を超える場合' do
+      it 'バリデーションが引っかかる' do
+        user = User.new(name: 'user', email: 'user@example.com',
+                        password: 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUV')
+        expect(user).not_to be_valid
+      end
+    end
+  end
+end
