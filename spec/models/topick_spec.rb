8f778488c043ed83c1d36f1a89c1f110e403d46f
hayato <sakigi94@gmail.com>
2021-09-26 14:19:39 +0000

    追加:topickのモデルテスト終了

--- spec/models/topick_spec.rb
+++ spec/models/topick_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Topick, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  
+
+  describe '掲示板のバリデーションテスト' do
+    context '名前が空欄の場合' do
+      it 'バリデーションエラーになる' do
+        topick = Topick.new(content: '')
+        expect(topick).not_to be_valid
+      end
+    end
+
+    context '名前が255文字以上の場合' do
+      it 'バリデーションエラーになる' do
+      topick = Topick.new(content: '1 * 255') 
+        expect(topick).to be_invalid
+      end
+    end
+  end
 end
