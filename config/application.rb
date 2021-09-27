ef8d074c7258e8f8d355215b16a32ec86814adf6
hayato <sakigi94@gmail.com>
2021-09-27 18:47:16 +0000

    編集:herokuのエラー修正

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ module Portfolio
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.generators do |g|
+
+
+      config.action_controller.default_protect_from_forgery = false
+
       g.test_framework :rspec,
                       model_specs: true,
                       view_specs: false,
@@ -20,7 +24,6 @@ module Portfolio
   end
 end
 
-config.action_controller.default_protect_from_forgery = false
 class ApplicationController < ActionController::Base
   # protect_from_forgery に with オプションを渡さない場合は with: :null_session と同等
   protect_from_forgery
