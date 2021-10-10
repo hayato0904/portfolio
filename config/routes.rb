4d6d5beafff81cd63c426481e0642eabea7c386d
hayato <sakigi94@gmail.com>
2021-10-10 19:26:49 +0000

    追加:userのshowに遷移できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   resources :feeds
   post '/tops/guest_sign_in', to: 'tops#guest_sign_in'
   post '/tops/admin_guest_sign_in', to: 'tops#admin_guest_sign_in'
   root to: 'tops#index'
   resources :favorites, only: [:create, :destroy, :index]
   get '/users', to: 'users#index'
+  get '/users/show', to: 'users#show'  # 追加したけど違うかも
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get '/topicks', to: 'topicks#index'
