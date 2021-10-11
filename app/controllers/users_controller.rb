80081f45d2b66c871cc2d6e4f89915d7b584b7e5
hayato <sakigi94@gmail.com>
2021-10-11 07:58:06 +0000

    追加:マイページ一覧表示完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(image: params[:user][:image], self_lntroduction: params[:user][:self_lntroduction])
   end
 
   def show
-
   end
 
 end
