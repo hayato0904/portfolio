bb1628686d8679a6c8b7dd68dbc256ea5d35508d
hayato <sakigi94@gmail.com>
2021-10-12 11:33:43 +0000

    追加:Userの自己紹介

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:image, :image_cache)
+    params.require(:user).permit(:name, :image, :image_cache, :self_lntroduction)
   end
 
 end
