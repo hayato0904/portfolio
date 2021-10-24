ec2fdfb3854886a4f2e947f95ce1fb3e43121ebe
hayato <sakigi94@gmail.com>
2021-10-24 15:57:58 +0000

    編集:topicksのtdを真ん中に配置

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_fill: [150, 150, "Center"] # キャリアウェーブを使用している時はここで設定する。
+  process resize_to_fill: [120, 120, "Center"] # キャリアウェーブを使用している時はここで設定する。
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
